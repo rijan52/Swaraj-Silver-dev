51ff870501960fedbad8101bacb6faacbc2ebdc8
rijan52 <rijanbuddhacharya2@gmail.com>
2026-06-10 12:50:59 +0545

    Error fixed

--- Frontend/src/data/gallery.js
+++ Frontend/src/data/gallery.js
@@ -1,6 +1,8 @@
 import Gallery1 from "../assets/Gallery1.png";
+import Gallery2 from "../assets/Gallery2.png";
 export const galleryImages = [
-  Gallery1
- 
-
+  Gallery2,
+  "https://images.unsplash.com/photo-1617038220319-276d3cfab638?q=80&w=1200&auto=format&fit=crop",
+  Gallery1,
+  "https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?q=80&w=1200&auto=format&fit=crop",
 ];
